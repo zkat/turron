ecfd33c340183ebce826d2f4fd7cce5484a0255b
Kat Marchán <kzm@zkat.tech>
2021-08-10 19:02:24 -0400

    feat(view): nicer output for `view <> versions`

--- commands/ruget-cmd-view/src/lib.rs
+++ commands/ruget-cmd-view/src/lib.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+
+use nu_table::{draw_table, StyledString, Table, TextStyle, Theme};
 use nuget_api::v3::{NuGetClient, RegistrationIndex, RegistrationLeaf, Tags};
 use ruget_command::{
     async_trait::async_trait,
@@ -8,6 +11,7 @@ use ruget_command::{
     serde_json, RuGetCommand,
 };
 use ruget_common::{
+    chrono::Datelike,
     chrono_humanize::HumanTime,
     miette::Diagnostic,
     miette_utils::{DiagnosticResult as Result, IntoDiagnostic},
@@ -49,29 +53,79 @@ impl RuGetCommand for ViewCmd {
         } else {
             return Err(ViewError::InvalidPackageSpec.into());
         };
-        let versions = client.versions(&package_id).await?;
         match &self.field.as_deref() {
-            Some("versions") => self.print_versions(versions).await,
-            None => self.print_version_details(&client, package_id, &requested, versions).await,
+            Some("versions") => self.print_versions(&client, package_id).await,
+            None => {
+                self.print_version_details(&client, package_id, &requested)
+                    .await
+            }
             _ => Err(ViewError::InvalidAttribute.into()),
         }
     }
 }
 
 impl ViewCmd {
-    async fn print_versions(&self, versions: Vec<Version>) -> Result<()> {
+    async fn print_versions(&self, client: &NuGetClient, package_id: &str) -> Result<()> {
+        let index = client.registration(package_id).await?;
+        let mut versions = Vec::new();
+        for page in index.items {
+            let page = if page.items.is_some() {
+                page
+            } else {
+                client.registration_page(&page.id).await?
+            };
+            for leaf in page
+                .items
+                .expect("RegistrationPage endpoints must have items!")
+                .into_iter()
+            {
+                versions.push((leaf.catalog_entry.version, leaf.catalog_entry.published));
+            }
+        }
         if self.json && !self.quiet {
+            let mut map = HashMap::new();
+            for (version, published) in versions {
+                map.insert(version, published);
+            }
             println!(
                 "{}",
-                serde_json::to_string_pretty(&versions)
+                serde_json::to_string_pretty(&map)
                     .into_diagnostic(&"ruget::view::json_serialization")?
             );
         } else if !self.quiet {
-            println!("Versions available for {}:", self.package);
-            println!();
-            for version in versions.iter().rev() {
-                println!("{}", version);
-            }
+            let headers = vec!["version", "published_at"]
+                .iter()
+                .map(|h| StyledString::new(h.to_string(), TextStyle::default_header()))
+                .collect::<Vec<StyledString>>();
+            let rows = versions
+                .iter()
+                .map(|(v, p)| {
+                    vec![
+                        StyledString::new(v.to_string(), TextStyle::basic_left()),
+                        StyledString::new(
+                            p.map(|p| {
+                                if p.year() > 1900 {
+                                    HumanTime::from(p).to_string()
+                                } else {
+                                    "unlisted".into()
+                                }
+                            })
+                            .unwrap_or_else(|| "unlisted".into()),
+                            TextStyle::basic_left(),
+                        ),
+                    ]
+                })
+                .collect::<Vec<Vec<StyledString>>>();
+            let width = if let Some((w, _)) = term_size::dimensions() {
+                w
+            } else {
+                80
+            };
+            let table = Table::new(headers, rows, Theme::rounded());
+            let color_hm: HashMap<String, nu_ansi_term::Style> = HashMap::new();
+            let output_table = draw_table(&table, width, &color_hm);
+            // Draw the table
+            println!("{}", output_table);
         }
         Ok(())
     }
@@ -81,8 +135,8 @@ impl ViewCmd {
         client: &NuGetClient,
         package_id: &str,
         requested: &VersionReq,
-        versions: Vec<Version>,
     ) -> Result<()> {
+        let versions = client.versions(&package_id).await?;
         let version = self.pick_version(package_id, requested, versions).await?;
         let (index, leaf) = self
             .find_version(client, package_id, requested, &version)
@@ -302,11 +356,13 @@ impl Diagnostic for ViewError {
 
     fn help(&self) -> Option<&(dyn std::fmt::Display)> {
         match self {
-            ViewError::InvalidPackageSpec => None,
-            ViewError::VersionNotFound(_, _) => Some(&"Try running `ruget view <id> versions`"),
-            ViewError::InvalidAttribute => Some(&"Use `ruget view --help` to see what attributes are supported"),
             // TODO: I guess this is good motivation to change miette...
             // ViewError::VersionNotFound(id, _) => Some(&format!("Try running `ruget view {} versions`", id))
+            ViewError::InvalidPackageSpec => None,
+            ViewError::VersionNotFound(_, _) => Some(&"Try running `ruget view <id> versions`"),
+            ViewError::InvalidAttribute => {
+                Some(&"Use `ruget view --help` to see what attributes are supported")
+            }
         }
     }
 }
